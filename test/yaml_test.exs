531a48ab0fc62c583cb2ae3329a5a785f07595d9
Syed Rizwan <syed.rizwan.gilani4400@gmail.com>
2025-11-19 09:14:59 +0500

    test: add TDD test suite for YAML.decode (single & multi-document)

--- test/yaml_test.exs
+++ test/yaml_test.exs
@@ -2,11 +2,179 @@ defmodule YAMLTest do
   use ExUnit.Case
   doctest YAML
 
-  test "decodes YAML and extracts invoice" do
-    yaml = """
-    invoice: 34843
-    """
+  @yaml_list """
+  - name: Ali
+    age: 25
+  - name: Sara
+    age: 22
+  """
 
-    assert {:ok, %{"invoice" => 34843}} = YAML.decode(yaml)
+  @yaml_map """
+  name: Ali
+  age: 25
+  hobbies:
+    - Reading
+    - Hiking
+    - Photography
+  """
+
+  @yaml_multi_list """
+  - name: Ali
+    age: 25
+  - name: Sara
+    age: 22
+  ---
+  - name: Rizwan
+    age: 24
+  """
+
+  @yaml_multi_map """
+  name: Ali
+  age: 25
+  ---
+  - name: Rizwan
+    age: 24
+  """
+
+  @tag group: :single
+  describe "decode/2 Single-Document -- input: list[]" do
+    setup do
+      %{yaml: @yaml_list}
+    end
+
+    test "Without option returns list of map (Default)", %{yaml: yaml} do
+      assert {:ok,
+              [
+                %{"name" => "Ali", "age" => 25},
+                %{"name" => "Sara", "age" => 22}
+              ]} = YAML.decode(yaml)
+    end
+
+    test "return: :first_document", %{yaml: yaml} do
+      assert {:ok,
+              [
+                %{"name" => "Ali", "age" => 25},
+                %{"name" => "Sara", "age" => 22}
+              ]} = YAML.decode(yaml, return: :first_document)
+    end
+
+    test "return: :all_documents", %{yaml: yaml} do
+      assert {:ok,
+              [
+                [
+                  %{"name" => "Ali", age: 25},
+                  %{"name" => "Sara", age: 22}
+                ]
+              ]} = YAML.decode(yaml, return: :all_documents)
+    end
+  end
+
+  @tag group: :single
+  describe "decode/2 Single-Document -- input: map %{}" do
+    setup do
+      %{yaml: @yaml_map}
+    end
+
+    test "Without option returns map (Default)", %{yaml: yaml} do
+      assert {:ok,
+              %{
+                "name" => "Ali",
+                "age" => 25,
+                "hobbies" => ["Reading", "Hiking", "Photography"]
+              }} = YAML.decode(yaml)
+    end
+
+    test "return: :first_document", %{yaml: yaml} do
+      assert {:ok,
+              %{
+                "name" => "Ali",
+                "age" => 25,
+                "hobbies" => ["Reading", "Hiking", "Photography"]
+              }} = YAML.decode(yaml, return: :first_document)
+    end
+
+    test "return: :all_documents wraps map in list", %{yaml: yaml} do
+      assert {:ok,
+              [
+                %{
+                  "name" => "Ali",
+                  "age" => 25,
+                  "hobbies" => ["Reading", "Hiking", "Photography"]
+                }
+              ]} = YAML.decode(yaml, return: :all_documents)
+    end
+  end
+
+  @tag group: :multi
+  describe "decode/2 Multi-Document -- input: list + list" do
+    setup do
+      %{yaml: @yaml_multi_list}
+    end
+
+    test "Without option returns both documents (Default)", %{yaml: yaml} do
+      assert {:ok,
+              [
+                [
+                  %{"name" => "Ali", "age" => 25},
+                  %{"name" => "Sara", "age" => 22}
+                ],
+                [
+                  %{"name" => "Rizwan", "age" => 24}
+                ]
+              ]} = YAML.decode(yaml)
+    end
+
+    test "return: :first_document returns only first document", %{yaml: yaml} do
+      assert {:ok,
+              [
+                %{"name" => "Ali", "age" => 25},
+                %{"name" => "Sara", "age" => 22}
+              ]} = YAML.decode(yaml, return: :first_document)
+    end
+
+    test "return: :all_documents same as default", %{yaml: yaml} do
+      assert {:ok,
+              [
+                [
+                  %{"name" => "Ali", "age" => 25},
+                  %{"name" => "Sara", "age" => 22}
+                ],
+                [
+                  %{"name" => "Rizwan", "age" => 24}
+                ]
+              ]} = YAML.decode(yaml, return: :all_documents)
+    end
+  end
+
+  @tag group: :multi
+  describe "decode/2 Multi-Document -- input: map + list" do
+    setup do
+      %{yaml: @yaml_multi_map}
+    end
+
+    test "Without option returns both documents (Default)", %{yaml: yaml} do
+      assert {:ok,
+              [
+                %{"name" => "Ali", "age" => 25},
+                [
+                  %{"name" => "Rizwan", "age" => 24}
+                ]
+              ]} = YAML.decode(yaml)
+    end
+
+    test "return: :first_document returns only first map document", %{yaml: yaml} do
+      assert {:ok, %{"name" => "Ali", "age" => 25}} =
+               YAML.decode(yaml, return: :first_document)
+    end
+
+    test "return: :all_documents (same as default)", %{yaml: yaml} do
+      assert {:ok,
+              [
+                %{"name" => "Ali", "age" => 25},
+                [
+                  %{"name" => "Rizwan", "age" => 24}
+                ]
+              ]} = YAML.decode(yaml, return: :all_documents)
+    end
   end
 end
